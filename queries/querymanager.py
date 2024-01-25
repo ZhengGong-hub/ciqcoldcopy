2145ee23b678a420ab50c0a24ba7db7cefa5f57c
zheng gong <zheng.gong94@gmail.com>
2024-01-25 21:29:57 +0100

    update

--- queries/querymanager.py
+++ queries/querymanager.py
@@ -1,5 +1,100 @@
-def query_3_sql_3(ls_ids, startdatestr, enddatestr):
-    return f"""
+import pandas as pd 
+import psycopg2
+import json
+
+def read_sql_to_df(sql):
+    """Internal: execute sql and get dataframe as a return 
+    
+    Args:
+        sql (str): sql to be executed 
+        db (psycopg2.connect): connect to ciq target database 
+        cursor (database connection.cursor): 
+    
+    Returns:
+        pd.DataFrame: contains result for executed sql 
+    """
+    connect_info = json.load(open('cfg/aws.json', 'r'))
+    connection = get_connection(connect_info)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(sql)
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        return pd.DataFrame(data,columns = columns)
+    except (Exception, psycopg2.DatabaseError) as error:
+        connection.rollback()
+        print("Error: %s" % error)
+
+
+def get_connection(dbInfo):
+    """get the db connection using info in dbInfo json file
+    
+    Args:
+        dbInfo (.json): user-pw for ciq target database 
+    
+    Returns:
+        psycopg2.connect
+    """
+    # config parameters of db connection
+
+    db = psycopg2.connect(
+        host=dbInfo['host'],
+        database=dbInfo["dbname"],
+        user=dbInfo["user"],
+        password=dbInfo["pwd"],
+        port=dbInfo['port'])
+    
+
+    print('DB connection set up!\n')
+    return db
+
+
+def estimatesQ(ls_ids, dataitemids, datestart):
+    sql= f"""
+    CREATE TEMP TABLE PC AS
+         SELECT 
+            -- EP.estimatepriodid,
+            -- EP.fiscalchainseriesid,
+            -- EP.periodtypeid,
+            EP.companyid,
+            EP.fiscalquarter,
+            EP.fiscalyear,
+            EP.calendarquarter,
+            EP.calendaryear,
+            EP.periodenddate,
+            EP.advancedate,
+                EC.tradingitemid,
+                EC.estimateConsensusId
+         FROM ciqEstimatePeriod EP
+                JOIN ciqEstimateConsensus   EC ON EC.estimatePeriodId = EP.estimatePeriodId
+         WHERE EP.companyId IN ({', '.join([str(id) for id in ls_ids])})
+               AND EP.periodTypeId = 2 -- Quarter 
+               AND EP.periodenddate > '{datestart}';
+    CREATE INDEX idx_pc ON PC (estimateConsensusId);
+
+    SELECT
+           PC.*,
+           D.dataitemname,
+           ED.dataitemid,
+           ED.currencyId,
+           FX.isocode,
+           ED.dataItemValue,
+           ED.effectiveDate,
+           ED.toDate,
+           ED.estimatescaleid,
+           SCALE.estimatescalename
+    FROM ciqEstimateNumericData ED
+       left JOIN ciqdataitem as D on ED.dataitemid = D.dataitemId -- explanational table
+       left JOIN ciqcurrency as FX on FX.currencyid = ED.currencyId -- explanational table
+       left JOIN ciqestimatescaletype as SCALE on SCALE.estimatescaleid = ED.estimatescaleid -- explanational table
+        JOIN PC ON ED.estimateConsensusId = PC.estimateConsensusId
+    WHERE ED.dataItemId IN ({', '.join([str(id) for id in dataitemids])});
+    """
+    return read_sql_to_df(sql)
+
+def get_transcript(ls_ids, startdatestr, enddatestr):
+    sql = f"""
     CREATE TEMP TABLE t_transcriptId AS
         SELECT DISTINCT a.transcriptId FROM targetskma.ciqTranscript a, (
             SELECT keyDevId, Min(transcriptCreationDateUTC) AS minTm FROM targetskma.ciqTranscript
@@ -114,3 +209,4 @@ def query_3_sql_3(ls_ids, startdatestr, enddatestr):
     JOIN targetskma.ciqTranscriptSpeakerType pt ON pt.speakerTypeId = t.speakerTypeId
     JOIN targetskma.ciqProfessional          pb ON pb.proId= t.proId;
     """
+    return read_sql_to_df(sql)
--- test.py
+++ test.py
@@ -1,29 +1,7 @@
 
 import pandas as pd
-from queries.query_1_sql import query_1_sql
 import json
-from queries.query_2_sql import estimatesQ
-# from query_3_sql import query_3_sql, query_3_sql_2, query_3_sql_3
-
-
-def query_1(companyids):
-    calyears = [2019, 2020, 2021, 2022, 2023]
-    return query_1_sql(companyids, calyears)
-
-def query_3(companyids, flag=False):
-    start, end = '2023-01-01 00:00:00', '2023-12-31 23:59:59'
-    if flag == 3:
-        return query_3_sql_3(companyids, start, end)
-    elif flag:
-        return query_3_sql_2(companyids, start, end)
-    else:
-        return query_3_sql(companyids, start, end)
-
-def query_3_2(companyids):
-    return query_3(companyids, flag=True)
-
-def query_3_3(companyids):
-    return query_3(companyids, flag=3)
+from queries.querymanager import estimatesQ, get_transcript
 
 if __name__ == "__main__":
 
@@ -39,5 +17,9 @@ if __name__ == "__main__":
 
     df = estimatesQ(companyids, dataitemids, datestart)
     df.to_csv('data/query2.csv')
+    print(df)
+
+    df = get_transcript(ls_ids = [24937, ], startdatestr = '2023-01-01', enddatestr='2025-01-01')
+    df.to_csv('data/query3.csv')
     print(df)
         
